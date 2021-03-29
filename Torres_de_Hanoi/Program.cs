23f0121729779ff6679105a6c2cc98462c474b27
Ivavil <56842906+Ivavil@users.noreply.github.com>
2021-03-29 15:38:24 +0200

    Creada la clase Program

--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -10,10 +10,40 @@ namespace Torres_de_Hanoi
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Introduce el número de elementos");
+            int i = Convert.ToInt32(Console.ReadLine());
+            if(i > 0) { 
+                List<Disco> lista = new List<Disco>();
+                for(int n = i; n >= 1; n--)
+                {
+                    lista.Add(new Disco(n));
+                }
+       
+                Pila INI = new Pila(i, lista[i - 1].Valor, lista, "ini");
+                Pila AUX = new Pila("aux");
+                Pila FIN = new Pila("fin");
 
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+                Console.WriteLine("Indica con qué método quieres resolver: 1-Iterativo 2-Recursivo");
+                int j = Convert.ToInt32(Console.ReadLine());
+                if (j == 1)
+                {
+                    Hanoi hanoi = new Hanoi();
+                    int res = hanoi.iterativo(i, INI, FIN, AUX);
+                    Console.WriteLine(res);
+                } else if(j == 2)
+                {
+                    Hanoi hanoi = new Hanoi();
+                    int res = hanoi.recursivo(i, INI, FIN, AUX);
+                    Console.WriteLine(res);
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, seleccione uno de los métodos anteriores");
+                }
+            } else
+            {
+                Console.WriteLine("Por favor, introduzca una cantidad de discos mayor que 0");
+            }
         }
     }
 }
