915f7e339c0ad2fe26cc8bd8fa7cc9f4fa182518
Ivavil <56842906+Ivavil@users.noreply.github.com>
2021-03-29 15:33:43 +0200

    Clase Pila creada

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -8,35 +8,99 @@ namespace Torres_de_Hanoi
 {
     class Pila
     {
-        public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
-        public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
-        public List<Disco> Elementos { get; set; }
-        */
+        private int size;
+        private int top;
+        private List<Disco> elementos;
+        private String name;
+        public int Size {
+            get
+            {
+                return this.size;
+            }
+            set
+            {
+                this.size = value;
+            }
+        }
+        public int Top {
+            get
+            {
+                return this.top;
+            }
+            set
+            {
+                this.top = value;
+            }
+        }
+        public List<Disco> Elementos {
+            get
+            {
+                return this.elementos;
+            }
+            set
+            {
+                this.elementos = value;
+            }
+        }
 
-        /* TODO: Implementar métodos */
-        public Pila()
+        public String Name
         {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
 
+        public Pila(String name)
+        {
+            this.size = 0;
+            this.top = 0;
+            this.elementos = new List<Disco>();
+            this.name = name;
+        }
+   
+        public Pila(int size, int top, List<Disco> elementos, String name)
+        {
+            this.size = size;
+            this.top = top;
+            this.elementos = elementos;
+            this.name = name;
         }
 
         public void push(Disco d)
         {
+            this.elementos.Add(d);
+            this.size++;
+            this.top = d.Valor;
+            
 
         }
 
         public Disco pop()
         {
+            if(this.size > 0) {
+                this.elementos.RemoveAt(this.size - 1);
+                this.size--;
+                if(this.size > 0) {
+                    this.top = this.elementos[this.size - 1].Valor;
+                } else if (this.size == 0)
+                {
+                    this.top = 0;
+                }
+                
+                
+            }
             return null;
         }                
 
         public bool isEmpty()
         {
-            return true;
+            if(this.elementos.Count == 0) { return true; }
+            return false;
         }
 
     }
